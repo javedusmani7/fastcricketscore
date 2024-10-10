6f7bfe8bc46bc37217c45637bb5b2ece958415e6
harvansh-emincence <harvansh.sainy@eminenceinnovation.com>
2024-10-10 12:11:27 +0530

    sync route created

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/mydatabase', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
   } catch (err) {
