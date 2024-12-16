1cc49e2a2a85f5b1acd9af69b4bd472936530ef5
Javed Usmani <javedusmani7@gmail.com>
2024-12-16 14:55:39 +0530

    changing post method to PUT method

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -8,11 +8,11 @@ router.post('/update_season_status/:sid', updateSeasonStatus);
 
 // competitions routes for admin dashboard
 router.get('/competitions', getCompetitions);
-router.post('/update_competition/:cid', updateCompetition);
+router.put('/update_competition/:cid', updateCompetition);
 router.get('/sync_competition_matches_by_cid', synccompetitionMatchesByCid);
 
 // matches routes for admin dashboard
-router.post('/update_match/:match_id', updateMatch);
+router.put('/update_match/:match_id', updateMatch);
 router.get('/matches', getMatches);
 
 // intervals routes for admin dashboard
